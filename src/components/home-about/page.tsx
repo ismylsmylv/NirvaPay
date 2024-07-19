ecaa6d60bc4d69a4c057cd9c11dac1d6d65dc682
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-19 21:29:08 +0400

    breakpoints added

--- src/components/home-about/page.tsx
+++ src/components/home-about/page.tsx
@@ -18,15 +18,15 @@ function HomeAbout({}: Props) {
             <h1>register</h1>
             <p>Register yourself as a customer</p>
           </div>
-          <div className="dashed"></div>
+          {/* <div className="dashed dashed1"></div> */}
           <div className="elem">
-            <div className="number">3</div>
+            <div className="number">2</div>
             <h1>add new card</h1>
             <p>Card for you to use our services</p>
           </div>
-          <div className="dashed"></div>
+          {/* <div className="dashed dashed2"></div> */}
           <div className="elem">
-            <div className="number">1</div>
+            <div className="number">3</div>
             <h1>success</h1>
             <p>Use NirvaPay in peace</p>
           </div>
