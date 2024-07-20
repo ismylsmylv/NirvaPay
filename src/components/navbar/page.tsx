7ff8b41a2652a5fd8b828219e8e1481ded045fcd
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-20 17:11:47 +0400

    navbar style updated

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RiDashboardLine } from "react-icons/ri";
-import { IoWalletOutline } from "react-icons/io5";
+import { RiWallet3Line } from "react-icons/ri";
 import { GrTransaction } from "react-icons/gr";
 import { IoSettingsOutline } from "react-icons/io5";
 import { useParams } from "next/navigation";
@@ -9,28 +9,41 @@ import { CgProfile } from "react-icons/cg";
 import "./style.scss";
 import LogoImg from "@/assets/img/logo.png";
 import Image from "next/image";
+import Link from "next/link";
 type Props = {};
 const navs = [
-  { icon: <RiDashboardLine />, name: "Dashboard", url: "/" },
-  { icon: <IoWalletOutline />, name: "My Wallet", url: "/wallet" },
-  { icon: <GrTransaction />, name: "Transactions", url: "/transactions" },
-  { icon: <FaRegBell />, name: "Notifications", url: "/settings" },
-  { icon: <IoSettingsOutline />, name: "Settings", url: "/settings" },
-  { icon: <CgProfile />, name: "Settings", url: "/profile" },
+  { icon: <RiDashboardLine size={25} />, name: "Dashboard", url: "/" },
+  { icon: <RiWallet3Line size={25} />, name: "My Wallet", url: "/wallet" },
+  {
+    icon: <GrTransaction size={25} />,
+    name: "Transactions",
+    url: "/transactions",
+  },
+  { icon: <FaRegBell size={25} />, name: "Notifications", url: "/settings" },
+  { icon: <IoSettingsOutline size={25} />, name: "Settings", url: "/settings" },
+  // { icon: <CgProfile />, name: "Settings", url: "/profile" },
 ];
 function Navbar({}: Props) {
   return (
     <div className="Navbar">
-      sdgdf
-      <Image alt="logo" src={LogoImg} />
-      <div className="list">
-        {navs.map((nav) => {
-          return (
-            <div className="nav" key={nav.name}>
-              {nav.name}
-            </div>
-          );
-        })}
+      <div className="container">
+        <Image alt="logo" src={LogoImg} />
+        <div className="list">
+          {navs.map((nav) => {
+            return (
+              <Link href={nav.url} className="nav" key={nav.name}>
+                {nav.icon}
+              </Link>
+            );
+          })}
+          <div className="profile">
+            <h1>Hello, Thomas</h1>
+            <img
+              src="https://www.rcwlitagency.com/media/5954/stagg-1.jpg"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
