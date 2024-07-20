69c4ffe03cd39e58a6d61d6e97ecb1316c7d059a
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-20 14:05:47 +0400

    navbar updated

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -4,6 +4,8 @@ import { IoWalletOutline } from "react-icons/io5";
 import { GrTransaction } from "react-icons/gr";
 import { IoSettingsOutline } from "react-icons/io5";
 import { useParams } from "next/navigation";
+import { FaRegBell } from "react-icons/fa6";
+import { CgProfile } from "react-icons/cg";
 import "./style.scss";
 import LogoImg from "@/assets/img/logo.png";
 import Image from "next/image";
@@ -12,12 +14,24 @@ const navs = [
   { icon: <RiDashboardLine />, name: "Dashboard", url: "/" },
   { icon: <IoWalletOutline />, name: "My Wallet", url: "/wallet" },
   { icon: <GrTransaction />, name: "Transactions", url: "/transactions" },
+  { icon: <FaRegBell />, name: "Notifications", url: "/settings" },
   { icon: <IoSettingsOutline />, name: "Settings", url: "/settings" },
+  { icon: <CgProfile />, name: "Settings", url: "/profile" },
 ];
 function Navbar({}: Props) {
   return (
     <div className="Navbar">
+      sdgdf
       <Image alt="logo" src={LogoImg} />
+      <div className="list">
+        {navs.map((nav) => {
+          return (
+            <div className="nav" key={nav.name}>
+              {nav.name}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
