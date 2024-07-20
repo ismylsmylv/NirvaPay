a53932a16c27538f5cc197aabe5e85934302c54d
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-20 13:41:47 +0400

    sidebar removed

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { RiDashboardLine } from "react-icons/ri";
+import { IoWalletOutline } from "react-icons/io5";
+import { GrTransaction } from "react-icons/gr";
+import { IoSettingsOutline } from "react-icons/io5";
+import { useParams } from "next/navigation";
+import "./style.scss";
+import LogoImg from "@/assets/img/logo.png";
+import Image from "next/image";
 type Props = {};
-
+const navs = [
+  { icon: <RiDashboardLine />, name: "Dashboard", url: "/" },
+  { icon: <IoWalletOutline />, name: "My Wallet", url: "/wallet" },
+  { icon: <GrTransaction />, name: "Transactions", url: "/transactions" },
+  { icon: <IoSettingsOutline />, name: "Settings", url: "/settings" },
+];
 function Navbar({}: Props) {
-  return <div>Navbar</div>;
+  return (
+    <div className="Navbar">
+      <Image alt="logo" src={LogoImg} />
+    </div>
+  );
 }
 
 export default Navbar;
