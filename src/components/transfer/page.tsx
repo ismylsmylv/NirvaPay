d3e1436e3c0f4187fd29412b03b6d6125ae76f78
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-21 21:11:48 +0400

    transactions added, pending fixes

--- src/components/transfer/page.tsx
+++ src/components/transfer/page.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-
+import "./style.scss";
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
 type Props = {};
 
 function Transfer({}: Props) {
-  return <div>Transfer</div>;
+  return (
+    <div className="Transfer">
+      <button>
+        <div className="texts">
+          <h1>top up</h1>
+          <p>Instantly Fund Your Account</p>
+        </div>
+        <FaArrowUp size={40} />
+      </button>
+      <button>
+        <div className="texts">
+          <h1>withdraw</h1>
+          <p>Fast and Secure Withdrawals</p>
+        </div>
+        <FaArrowDown size={40} />
+      </button>
+    </div>
+  );
 }
 
 export default Transfer;
