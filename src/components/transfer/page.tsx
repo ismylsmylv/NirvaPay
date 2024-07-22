da6e7a25e857f45c2e2ad8a16264a14e1f8ed9f5
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-22 14:16:09 +0400

    transfer component completed

--- src/components/transfer/page.tsx
+++ src/components/transfer/page.tsx
@@ -2,24 +2,37 @@ import React from "react";
 import "./style.scss";
 import { FaArrowUp } from "react-icons/fa";
 import { FaArrowDown } from "react-icons/fa";
+
+import { FaLongArrowAltDown, FaLongArrowAltUp } from "react-icons/fa";
+import "./style.scss";
 type Props = {};
 
 function Transfer({}: Props) {
   return (
     <div className="Transfer">
-      <button>
+      <button className="left">
+        <div className="back">
+          <FaArrowUp size={130} />
+        </div>
         <div className="texts">
           <h1>top up</h1>
           <p>Instantly Fund Your Account</p>
         </div>
-        <FaArrowUp size={40} />
+        <div className="icon">
+          <FaLongArrowAltUp size={30} color="#6119C5" />
+        </div>
       </button>
-      <button>
+      <button className="right">
         <div className="texts">
           <h1>withdraw</h1>
           <p>Fast and Secure Withdrawals</p>
         </div>
-        <FaArrowDown size={40} />
+        <div className="back">
+          <FaArrowDown size={130} color="#3A0182" />
+        </div>
+        <div className="icon">
+          <FaLongArrowAltDown size={30} color="#3A0182" />
+        </div>
       </button>
     </div>
   );
