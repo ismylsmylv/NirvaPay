8d09bf42711efe0aef80912b6fb66ec9b5cea71e
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-23 11:11:28 +0400

    chart fixes

--- src/components/chart/page.tsx
+++ src/components/chart/page.tsx
@@ -1,99 +1,57 @@
 "use client";
-import "./style.scss";
-import Stack from "@mui/material/Stack";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Radio from "@mui/material/Radio";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { axisClasses } from "@mui/x-charts/ChartsAxis";
+import "./style.scss";
 // import { dataset } from "../dataset/weather";
 import React from "react";
 // const dataset = [{ month: "june", band: 200 }];
 const dataset = [
   {
-    london: 59,
-    paris: 57,
-    newYork: 86,
-    seoul: 21,
+    amount: 59,
     month: "Jan",
   },
   {
-    london: 50,
-    paris: 52,
-    newYork: 78,
-    seoul: 28,
+    amount: 50,
     month: "Feb",
   },
   {
-    london: 47,
-    paris: 53,
-    newYork: 106,
-    seoul: 41,
+    amount: 47,
     month: "Mar",
   },
   {
-    london: 54,
-    paris: 56,
-    newYork: 92,
-    seoul: 73,
+    amount: 54,
     month: "Apr",
   },
   {
-    london: 57,
-    paris: 69,
-    newYork: 92,
-    seoul: 99,
+    amount: 57,
     month: "May",
   },
   {
-    london: 60,
-    paris: 63,
-    newYork: 103,
-    seoul: 144,
+    amount: 60,
     month: "June",
   },
   {
-    london: 59,
-    paris: 60,
-    newYork: 105,
-    seoul: 319,
+    amount: 59,
     month: "July",
   },
   {
-    london: 65,
-    paris: 60,
-    newYork: 106,
-    seoul: 249,
+    amount: 65,
     month: "Aug",
   },
   {
-    london: 51,
-    paris: 51,
-    newYork: 95,
-    seoul: 131,
+    amount: 51,
     month: "Sept",
   },
   {
-    london: 60,
-    paris: 65,
-    newYork: 97,
-    seoul: 55,
+    amount: 60,
     month: "Oct",
   },
   {
-    london: 67,
-    paris: 64,
-    newYork: 76,
-    seoul: 48,
+    amount: 67,
     month: "Nov",
   },
   {
-    london: 61,
-    paris: 70,
-    newYork: 103,
-    seoul: 25,
+    amount: 61,
     month: "Dec",
   },
 ];
@@ -102,10 +60,10 @@ const valueFormatter = (value: number | null) => `${value}mm`;
 const chartSetting = {
   yAxis: [
     {
-      label: "rainfall (mm)",
+      // label: "rainfall (mm)",
     },
   ],
-  series: [{ dataKey: "seoul", label: "Seoul rainfall", valueFormatter }],
+  series: [{ dataKey: "amount", label: "Seoul rainfall", valueFormatter }],
   height: 300,
   sx: {
     [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
@@ -115,13 +73,8 @@ const chartSetting = {
 };
 
 export default function Charts() {
-  const [tickPlacement, setTickPlacement] = React.useState<
-    "start" | "end" | "middle" | "extremities"
-  >("middle");
-  const [tickLabelPlacement, setTickLabelPlacement] = React.useState<
-    "middle" | "tick"
-  >("middle");
-
+  const tickPlacement = "middle";
+  const tickLabelPlacement = "middle";
   return (
     <div style={{ width: "100%" }} className="Charts">
       <BarChart
@@ -132,6 +85,7 @@ export default function Charts() {
             dataKey: "month",
             tickPlacement,
             tickLabelPlacement,
+            hideTooltip: true,
           },
         ]}
         {...chartSetting}
