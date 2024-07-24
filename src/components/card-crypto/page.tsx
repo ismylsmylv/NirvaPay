49f05c3c9c80cfb8a3b7b8fc09ca95be00f12eb7
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-07-24 09:05:17 +0400

    crypto balances addeD

--- src/components/card-crypto/page.tsx
+++ src/components/card-crypto/page.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import { toast, ToastContainer } from "react-toastify";
+import { FaClipboard } from "react-icons/fa";
+import { FaClipboardCheck } from "react-icons/fa";
 type Props = {
   crypto: {
     name: string;
     img: string;
     balance: string;
     address: string;
+    short: string;
   };
 };
 
 function CardCrypto({ crypto }: Props) {
+  const [copy, setcopy] = useState(false);
   return (
     <>
-      <ToastContainer />
-      <div className="CardCrypto">
+      <div
+        className="CardCrypto"
+        onMouseLeave={() => {
+          setcopy(false);
+        }}
+      >
         <div className="content">
           <div className="name">{crypto.name}</div>
-          <div className="balance">{crypto.balance}</div>
+          <div className="balance">
+            <div className="count">
+              {crypto.balance} {crypto.short}
+            </div>
+          </div>
           <button
             onClick={() => {
+              setcopy(true);
               navigator.clipboard.writeText(crypto.address);
               toast.info(`Address copied to clipboard`, {
                 position: "bottom-right",
@@ -33,7 +46,15 @@ function CardCrypto({ crypto }: Props) {
               });
             }}
           >
-            copy address
+            {copy ? (
+              <>
+                <FaClipboardCheck color="gray" /> Address copied
+              </>
+            ) : (
+              <>
+                <FaClipboard color="gray" /> Copy Adress
+              </>
+            )}
           </button>
         </div>
         <img src={crypto.img} alt="" />
--- src/components/cards-crypto/page.tsx
+++ src/components/cards-crypto/page.tsx
@@ -8,6 +8,7 @@ const cryptos = [
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/800px-Bitcoin.svg.png",
     balance: "0.9",
     address: "1Lbcfr7sAHTD9CgdQo3HTMTkV8LK4ZnX71",
+    short: "BTC",
   },
 
   {
@@ -15,12 +16,14 @@ const cryptos = [
     img: "https://i.pinimg.com/736x/de/49/55/de495504a51163548744bd1d7a0b499d.jpg",
     balance: "5.6",
     address: "0xb794f5ea0ba39494ce839613fffba74279579268",
+    short: "ETH",
   },
   {
     name: "ton",
     img: "https://upload.wikimedia.org/wikipedia/commons/3/3d/The_open_network_logo.svg",
     balance: "43",
     address: "EQAWzEKcdnykvXfUNouqdS62tvrp32bCxuKS6eQrS6ISgcLo",
+    short: "TON",
   },
 ];
 function CardsCrypto({}: Props) {
