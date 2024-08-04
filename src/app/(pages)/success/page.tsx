52081cae135cf98e2e9767e25d503ebebe96b150
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-08-04 09:43:56 +0400

    fixes

--- src/app/(pages)/success/page.tsx
+++ src/app/(pages)/success/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useAppSelector } from "@/redux/hooks/hooks";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -8,60 +9,65 @@ import "./style.scss";
 
 type Props = {};
 type Transaction = {
-  id: string;
-  amount: string;
-  from: string;
-  to: string;
-  date: string;
+  id?: string;
+  amount?: string;
+  from?: string;
+  to?: string;
+  date?: string;
 };
+
 function Success({}: Props) {
   const completedTransaction: Transaction | any = useAppSelector(
     (state) => state.transaction.completedTransaction
   );
   const router = useRouter();
-  const [transaction, settransaction] = useState<Transaction>({});
+  const [transaction, setTransaction] = useState<Transaction>({});
   const searchParams = useSearchParams();
+
   useEffect(() => {
     if (typeof window !== "undefined") {
-      settransaction(JSON.parse(searchParams.get("transaction")));
+      const transactionData = searchParams.get("transaction");
+      if (transactionData) {
+        setTransaction(JSON.parse(transactionData));
+      }
     }
     // !auth && router.push("/account/login"); UNCOMMENT LATER
   }, [searchParams, router]);
+
   return (
     <div className="Success container">
       <GoVerified size={40} fill="green" />
-      <h1>transaction successful</h1>
+      <h1>Transaction Successful</h1>
 
       <div className="line">
-        <div className="name">Receipt number</div>
+        <div className="name">Receipt Number</div>
         <div className="value">{transaction.id || "undefined"}</div>
       </div>
       <div className="line">
         <div className="name">Amount</div>
-        <div className="value">${transaction.amount}</div>
+        <div className="value">${transaction.amount || "undefined"}</div>
       </div>
       <div className="line">
-        <div className="name">Paid by</div>
-        <div className="value">{transaction.from}</div>
+        <div className="name">Paid By</div>
+        <div className="value">{transaction.from || "undefined"}</div>
       </div>
       <div className="line">
-        <div className="name">Enroll to</div>
-        <div className="value">{transaction.to}</div>
+        <div className="name">Enroll To</div>
+        <div className="value">{transaction.to || "undefined"}</div>
       </div>
       <div className="line">
         <div className="name">Date</div>
-        <div className="value">{transaction.date}</div>
+        <div className="value">{transaction.date || "undefined"}</div>
       </div>
       <button
         onClick={(e) => {
-          router.push("dashboard");
+          router.push("/dashboard");
         }}
       >
-        continue
+        Continue
       </button>
     </div>
   );
 }
 
 export default Success;
-// {"from":"5313779039123482","to":"4630141858671729","amount":20,"date":"2024-08-02T15:27:12.809Z"}
