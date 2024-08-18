5a3d09fc35d06bf054f00cf3bbdf36cf3b82bdb9
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-08-18 08:21:27 +0400

    charts updated

--- src/components/charts/page.tsx
+++ src/components/charts/page.tsx
@@ -3,21 +3,26 @@ import { useAppDispatch, useAppSelector } from "@/redux/hooks/hooks";
 import { fetchUserById } from "@/redux/slice/auth";
 import { Box, Stack } from "@mui/material";
 import { SparkLineChart } from "@mui/x-charts/SparkLineChart";
-import { formatDistanceToNow, parseISO } from "date-fns";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import "react-tooltip/dist/react-tooltip.css";
 import "./style.scss";
+
 type Props = {};
+
 type Userdatas = {
   notifications: any;
+  crypto: any;
 };
+
 function Charts({}: Props) {
   const dispatch = useAppDispatch();
+  const [numbers, setNumbers] = useState<number[]>([]);
   const userdatas: Userdatas[] = useAppSelector(
     (state) => state.auth.userdatas
   );
+
   const cryptos = [
     {
       name: "bitcoin",
@@ -26,7 +31,6 @@ function Charts({}: Props) {
       address: userdatas?.crypto?.bitcoin?.address,
       short: "BTC",
     },
-
     {
       name: "ethereum",
       img: "https://i.pinimg.com/736x/de/49/55/de495504a51163548744bd1d7a0b499d.jpg",
@@ -42,30 +46,26 @@ function Charts({}: Props) {
       short: "TON",
     },
   ];
-  let i = 0;
+
   useEffect(() => {
     dispatch(fetchUserById());
-  }, []);
+
+    function generateRandomArray() {
+      const array = [];
+      for (let i = 0; i < 8; i++) {
+        const randomNum = Math.floor(Math.random() * 10);
+        array.push(randomNum);
+      }
+      setNumbers(array);
+    }
+
+    generateRandomArray();
+    const intervalId = setInterval(generateRandomArray, 1000);
+    return () => clearInterval(intervalId);
+  }, [dispatch]);
+
   return (
     <div className="Charts container">
-      {/* <button
-        onClick={async () => {
-          await fetch(
-            new Request("https://api.livecoinwatch.com/coins/single"),
-            {
-              method: "POST",
-              headers: new Headers({
-                "content-type": "application/json",
-                "x-api-key": "2c4f4eb2-744a-4451-82c9-97f6cdaf36ce",
-              }),
-            }
-          ).then((res) => {
-            console.log(res);
-          });
-        }}
-      >
-        chart
-      </button> */}
       <div className="heading">
         <h1>Cryptos</h1>
         <Link href={`/transactions`}>
@@ -73,19 +73,17 @@ function Charts({}: Props) {
         </Link>
       </div>
       <div className="charts">
-        {cryptos.map((crypto) => {
-          return (
-            <Stack direction="row" sx={{ width: "100%" }} key={"result"}>
-              <Box sx={{ flexGrow: 1 }}>
-                <div className="head">
-                  <img src={crypto.img} alt="" />
-                  <p>{crypto.name}</p>
-                </div>
-                <SparkLineChart data={[1, 4, 2, 5, 7, 2, 4, 6]} height={100} />
-              </Box>
-            </Stack>
-          );
-        })}
+        {cryptos.map((crypto) => (
+          <Stack direction="row" sx={{ width: "100%" }} key={crypto.short}>
+            <Box sx={{ flexGrow: 1 }}>
+              <div className="head">
+                <img src={crypto.img} alt="" />
+                <p>{crypto.name}</p>
+              </div>
+              <SparkLineChart data={numbers} height={100} />
+            </Box>
+          </Stack>
+        ))}
       </div>
     </div>
   );
