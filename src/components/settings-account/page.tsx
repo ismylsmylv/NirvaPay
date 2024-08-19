08c5579bd38737a0c039fdba71e37274c0d5d714
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-08-19 18:23:11 +0400

    settings options refactored

--- src/components/settings-account/page.tsx
+++ src/components/settings-account/page.tsx
@@ -1,31 +1,42 @@
 import { Button } from "@mui/material";
 import "./style.scss";
 type Props = {};
-
+const settings = [
+  {
+    heading: "Log out",
+    sub: "Sign out and end your session",
+    button: "log out",
+  },
+  {
+    heading: "Suspend account",
+    sub: "Temporarily disable your account",
+    button: "suspend",
+  },
+  {
+    heading: "Delete account",
+    sub: "Permanently remove your account",
+    button: "delete",
+  },
+];
 function SettingsAccount({}: Props) {
   return (
     <section className="SettingsAccount">
       <h1>Manage account</h1>
       <ul>
-        <li>
-          <p>log out</p>
-          <button></button>
-          <Button variant="outlined" color="error">
-            log out
-          </Button>
-        </li>
-        <li>
-          <p>suspend account</p>
-          <Button variant="outlined" color="error">
-            suspend
-          </Button>
-        </li>
-        <li>
-          <p>delete account</p>
-          <Button variant="outlined" color="error">
-            delete
-          </Button>
-        </li>
+        {settings.map((setting) => {
+          return (
+            <li key={setting.heading}>
+              <div>
+                <h2>{setting.heading}</h2>
+                <p>{setting.sub}</p>
+              </div>
+              <button></button>
+              <Button variant="outlined" color="error">
+                {setting.button}
+              </Button>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
